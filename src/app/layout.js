d8b055f53bd426914107fe0639ba999386ede2a4
toriq <thoriqahmad17@gmail.com>
2024-05-17 09:57:58 +0700

    warna warni

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className}`}>
         <div className="container mx-auto flex justify-center items-center py-10">
           <Image
             src={`/fm_logo.png`}
--- src/components/PriceList.jsx
+++ src/components/PriceList.jsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import { FaCheckCircle } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Price = [
   {
@@ -11,6 +13,7 @@ const Price = [
       "Wifi Tanpa batas Kuota",
       "Harga Sudah ppn dan sewa alat",
     ],
+    class: ["border-blue-700", "text-blue-500"],
   },
   {
     name: "Happy Premium",
@@ -21,6 +24,7 @@ const Price = [
       "Wifi Tanpa batas Kuota",
       "Harga Sudah ppn dan sewa alat",
     ],
+    class: ["border-yellow-700", "text-yellow-500"],
   },
   {
     name: "Happy Ekonomi",
@@ -31,6 +35,7 @@ const Price = [
       "Wifi Tanpa batas Kuota",
       "Harga Sudah ppn dan sewa alat",
     ],
+    class: ["border-red-700", "text-red-500"],
   },
   {
     name: "Stream Pro",
@@ -41,6 +46,7 @@ const Price = [
       "Wifi Tanpa batas Kuota",
       "Harga Sudah ppn dan sewa alat",
     ],
+    class: ["border-green-700", "text-green-500"],
   },
   {
     name: "Gamming",
@@ -51,6 +57,7 @@ const Price = [
       "Wifi Tanpa batas Kuota",
       "Harga Sudah ppn dan sewa alat",
     ],
+    class: ["border-purple-700", "text-purple-500"],
   },
   {
     name: "Bisnis",
@@ -61,6 +68,7 @@ const Price = [
       "Wifi Tanpa batas Kuota",
       "Harga Sudah ppn dan sewa alat",
     ],
+    class: ["border-orange-700", "text-orange-500"],
   },
 ];
 
@@ -70,24 +78,35 @@ const PriceList = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {Price.map((item, idx) => {
           return (
-            <div
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 },
+              }}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 0.5, delay: 0.5 }}
               key={idx}
-              className="p-6 rounded-lg shadow-lg border-t-4 border-red-900"
+              className={`p-6 rounded-lg shadow-lg border-t-4 ${item.class[0]} bg-gray-300/50`}
             >
               <div className="flex flex-col">
-                <h1 className="text-xl text-red-700 uppercase font-bold">
+                <h1 className={`text-xl ${item.class[1]} uppercase font-bold`}>
                   {item.name}
                 </h1>
-                <div className="flex justify-center font-extrabold items-end gap-1 py-6">
-                  <div className=" flex text-4xl items-start gap-1">
-                    <p className="text-red-700 text-lg">Rp</p>
-                    <h1>{item.price}</h1>
+                <div className="grid grid-cols-1 font-extrabold  gap-1 py-6">
+                  <div className="flex justify-center items-end">
+                    <div className=" flex text-4xl items-start gap-1">
+                      <p className={`${item.class[1]} text-lg`}>Rp</p>
+                      <h1 className={`${item.class[1]} text-5xl`}>
+                        {item.price}
+                      </h1>
+                    </div>
+                    <div>
+                      <p className={`${item.class[1]} mb-1`}>000</p>
+                    </div>
                   </div>
-                  <div>
-                    <p className="text-red-700 mb-1">000</p>
-                  </div>
+                  <p className={`text-black text-md font-medium`}>Per Bulan</p>
                 </div>
-                {/* <h1>{item.price}</h1> */}
                 <div className="grid grid-cols-1 text-left">
                   {item.spec.map((item, idx) => {
                     return (
@@ -104,7 +123,7 @@ const PriceList = () => {
                   Daftar Sekarang
                 </button>
               </div>
-            </div>
+            </motion.div>
           );
         })}
       </div>
