a9aa71eb9c3a9b19358ae73a6341e60efd5e1b3b
toriq <thoriqahmad17@gmail.com>
2024-05-17 08:33:10 +0700

    add wa

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const ContactMe = () => {
   return (
     <>
-      <Link href={`#`} target="blank" className="fixed bottom-0 right-0 m-5 lg:m-10 p-3 text-4xl rounded-full bg-green-500 text-white">
+      <Link href={`https://wa.me/6287897042408`} target="blank" className="fixed bottom-0 right-0 m-5 lg:m-10 p-3 text-4xl rounded-full bg-green-500 text-white">
         <FaWhatsapp />
       </Link>
     </>
