0bc72c33fb897dfe9f020a70a3293838d0ecd78d
achmadtoriq <142966757+achmadtoriq@users.noreply.github.com>
2024-05-19 10:51:57 +0700

    Update PriceList.jsx
    add link

--- src/components/PriceList.jsx
+++ src/components/PriceList.jsx
@@ -306,9 +306,11 @@ const PriceList = () => {
                 </div>
               </div>
               <div className="text-center">
-                <button className="bg-green-500 text-white px-4 py-2 rounded-lg font-bold">
+
+<Link href={`https://wa.me/6287897042408`} target="blank" >                <button className="bg-green-500 text-white px-4 py-2 rounded-lg font-bold">
                   Daftar Sekarang
                 </button>
+</Link>
               </div>
             </motion.div>
           );
