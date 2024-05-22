e4713ceb3058765842c188c314f9d5413a42cd0e
toriq <thoriqahmad17@gmail.com>
2024-05-22 11:14:31 +0700

    main

--- src/components/PriceList.jsx
+++ src/components/PriceList.jsx
@@ -16,7 +16,7 @@ const Price = [
     istvkabel: false,
     cntchannel: "-",
     spec: ["Wifi Tanpa batas Kuota", "Harga Sudah ppn dan sewa alat"],
-    class: ["border-blue-700", "text-blue-500", "data-[selected]:bg-blue-500"]
+    class: ["border-blue-700", "text-blue-500", "data-[selected]:bg-blue-500"],
   },
   {
     id: 2,
@@ -26,7 +26,11 @@ const Price = [
     istvkabel: false,
     cntchannel: "-",
     spec: ["Wifi Tanpa batas Kuota", "Harga Sudah ppn dan sewa alat"],
-    class: ["border-yellow-700", "text-yellow-500", "data-[selected]:bg-yellow-500"]
+    class: [
+      "border-yellow-700",
+      "text-yellow-500",
+      "data-[selected]:bg-yellow-500",
+    ],
   },
   {
     id: 3,
@@ -36,7 +40,7 @@ const Price = [
     istvkabel: false,
     cntchannel: "-",
     spec: ["Wifi Tanpa batas Kuota", "Harga Sudah ppn dan sewa alat"],
-    class: ["border-red-700", "text-red-500", "data-[selected]:bg-red-500"]
+    class: ["border-red-700", "text-red-500", "data-[selected]:bg-red-500"],
   },
   {
     id: 4,
@@ -46,7 +50,11 @@ const Price = [
     istvkabel: true,
     cntchannel: 119,
     spec: ["Wifi Tanpa batas Kuota", "Harga Sudah ppn dan sewa alat"],
-    class: ["border-green-700", "text-green-500", "data-[selected]:bg-green-500"]
+    class: [
+      "border-green-700",
+      "text-green-500",
+      "data-[selected]:bg-green-500",
+    ],
   },
   {
     id: 5,
@@ -56,7 +64,11 @@ const Price = [
     istvkabel: true,
     cntchannel: 119,
     spec: ["Wifi Tanpa batas Kuota", "Harga Sudah ppn dan sewa alat"],
-    class: ["border-purple-700", "text-purple-500", "data-[selected]:bg-purple-500"]
+    class: [
+      "border-purple-700",
+      "text-purple-500",
+      "data-[selected]:bg-purple-500",
+    ],
   },
   {
     id: 6,
@@ -66,7 +78,11 @@ const Price = [
     istvkabel: true,
     cntchannel: 198,
     spec: ["Wifi Tanpa batas Kuota", "Harga Sudah ppn dan sewa alat"],
-    class: ["border-orange-700", "text-orange-500", "data-[selected]:bg-orange-500"]
+    class: [
+      "border-orange-700",
+      "text-orange-500",
+      "data-[selected]:bg-orange-500",
+    ],
   },
   {
     id: 7,
@@ -76,9 +92,40 @@ const Price = [
     istvkabel: true,
     cntchannel: 204,
     spec: ["Wifi Tanpa batas Kuota", "Harga Sudah ppn dan sewa alat"],
-    class: ["border-rose-700", "text-rose-500", "data-[selected]:bg-rose-500"]
+    class: ["border-rose-700", "text-rose-500", "data-[selected]:bg-rose-500"],
+  },
+];
+
+function getSpeed(speeds) {
+  let prosen = 0;
+  switch (speeds) {
+    case 30:
+      prosen = 50;
+      break;
+    case 50:
+      prosen = 60;
+      break;
+    case 100:
+      prosen = 80;
+      break;
+    case 150:
+      prosen = 85;
+      break;
+    case 200:
+      prosen = 90;
+      break;
+    case 300:
+      prosen = 95;
+      break;
+    case 500:
+      prosen = 100;
+      break;
+    default:
+      prosen = 0;
+      break;
   }
-]
+  return prosen;
+}
 
 const PriceList = () => {
   const [selectedIndex, setSelectedIndex] = useState({});
@@ -118,43 +165,17 @@ const PriceList = () => {
           <h1 className="text-2xl font-bold">Paket Internet Only</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {Price.map((item, idx) => {
-              const borderLine = dataColor[item.id - 1].border_color
-              const textColor = dataColor[item.id - 1].text_color
-              const selectedColor = dataColor[item.id - 1].selected_color
+              const borderLine = dataColor[item.id - 1].border_color;
+              const textColor = dataColor[item.id - 1].text_color;
+              const selectedColor = dataColor[item.id - 1].selected_color;
 
               let speeds = parseInt(item.speed.replace(" Mbps", ""));
-              let prosen = 0;
-              switch (speeds) {
-                case 30:
-                  prosen = 50;
-                  break;
-                case 50:
-                  prosen = 60;
-                  break;
-                case 100:
-                  prosen = 80;
-                  break;
-                case 150:
-                  prosen = 85;
-                  break;
-                case 200:
-                  prosen = 90;
-                  break;
-                case 300:
-                  prosen = 95;
-                  break;
-                case 500:
-                  prosen = 100;
-                  break;
-                default:
-                  prosen = 0;
-                  break;
-              }
+              let prosen = getSpeed(speeds);
 
               let gold_p = item.price[1]?.split(",");
               let stand_p = item.price[0].split(",");
 
-              return (
+              return item.istvkabel === false ? (
                 <motion.div
                   variants={{
                     hidden: { opacity: 0, y: 100 },
@@ -312,29 +333,190 @@ const PriceList = () => {
                     {/* </Link> */}
                   </div>
                 </motion.div>
+              ) : (
+                ""
               );
             })}
-            {/* <BoxPaket
-              dataPrice={inetOnly}
-              handleTabClick={handleTabClick}
-              selectedIndex={selectedIndex}
-              setIsOpen={setIsOpen}
-              setchoice={setchoice}
-            /> */}
           </div>
         </div>
-        {/* <div className="flex flex-col items-start justify-start my-5 gap-5">
+        <div className="flex flex-col items-start justify-start my-5 gap-5">
           <h1 className="text-2xl font-bold">Paket Internet TV</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 ">
-            <BoxPaket
-              dataPrice={inetTV}
-              handleTabClick={handleTabClick}
-              selectedIndex={selectedIndex}
-              setIsOpen={setIsOpen}
-              setchoice={setchoice}
-            />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            {Price.map((item, idx) => {
+              const borderLine = dataColor[item.id - 1].border_color;
+              const textColor = dataColor[item.id - 1].text_color;
+              const selectedColor = dataColor[item.id - 1].selected_color;
+
+              let speeds = parseInt(item.speed.replace(" Mbps", ""));
+              let prosen = getSpeed(speeds);
+
+              let gold_p = item.price[1]?.split(",");
+              let stand_p = item.price[0].split(",");
+
+              return item.istvkabel == true ? (
+                <motion.div
+                  variants={{
+                    hidden: { opacity: 0, y: 100 },
+                    visible: { opacity: 1, y: 0 },
+                  }}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{ duration: 0.5, delay: 0.5 }}
+                  key={idx}
+                  className={`p-6 rounded-lg shadow-lg border-t-4 ${borderLine} bg-gray-300/50`}
+                >
+                  <div className="flex flex-col">
+                    <h1
+                      className={`text-xl ${item.class[1]} uppercase font-bold`}
+                    >
+                      {item.name}
+                    </h1>
+                    {item.price.length > 1 ? (
+                      <>
+                        <TabGroup
+                          className={`text-sm p-3`}
+                          selectedIndex={selectedIndex[item.id] || 0}
+                          onChange={(index) => {
+                            handleTabClick(item.id, index);
+                            setIsGold(index);
+                          }}
+                        >
+                          <TabList
+                            className={`flex justify-center items-center`}
+                          >
+                            <Tab
+                              className={`${item.class[2]} data-[selected]:text-white data-[selected]:border-none bg-white border border-gray-300  py-1 px-4 rounded-l-lg ${item.class[1]}`}
+                            >
+                              <p className="font-bold">STANDAR</p>
+                              {/* <p>Per Bulan</p> */}
+                            </Tab>
+                            <Tab
+                              className={`${item.class[2]} data-[selected]:text-white data-[selected]:border-none bg-white border border-gray-300  py-1 px-4 rounded-r-lg ${item.class[1]}`}
+                            >
+                              <p className="font-bold">GOLD</p>
+                              {/* <p>12 Bulan</p> */}
+                            </Tab>
+                          </TabList>
+                          <TabPanels>
+                            <TabPanel
+                              className={`grid grid-cols-1 font-extrabold  gap-1 py-6`}
+                            >
+                              <div className="flex justify-center items-end">
+                                <div className=" flex text-4xl items-start gap-1">
+                                  <p className={`${item.class[1]} text-lg`}>
+                                    Rp
+                                  </p>
+                                  <h1 className={`${item.class[1]} text-5xl`}>
+                                    {stand_p[0]}.
+                                  </h1>
+                                </div>
+                                <div>
+                                  <p className={`${item.class[1]} text-xl`}>
+                                    {stand_p[1]}
+                                  </p>
+                                </div>
+                              </div>
+                              <p className={`text-black text-md font-medium`}>
+                                Per Bulan
+                              </p>
+                            </TabPanel>
+                            <TabPanel
+                              className={`grid grid-cols-1 font-extrabold  gap-1 py-6`}
+                            >
+                              <div className="flex justify-center items-end">
+                                <div className=" flex text-4xl items-start gap-1">
+                                  <p className={`${item.class[1]} text-lg`}>
+                                    Rp
+                                  </p>
+                                  <h1 className={`${item.class[1]} text-5xl`}>
+                                    {gold_p[0]}.
+                                  </h1>
+                                </div>
+                                <div>
+                                  <p className={`${item.class[1]} text-xl`}>
+                                    {gold_p[1]}
+                                  </p>
+                                </div>
+                              </div>
+                              <p className={`text-black text-md font-medium`}>
+                                Per Tahun
+                              </p>
+                            </TabPanel>
+                          </TabPanels>
+                        </TabGroup>
+                      </>
+                    ) : (
+                      <>
+                        <div className="text-sm p-3">
+                          <div
+                            className={`bg-blue-500 w-[50%] mx-auto text-center text-white border-none border border-gray-300 py-1 px-4 rounded-lg`}
+                          >
+                            <p className="font-bold">STANDAR</p>
+                            {/* <p>Per Bulan</p> */}
+                          </div>
+                          <div
+                            className={`grid grid-cols-1 font-extrabold  gap-1 py-6`}
+                          >
+                            <div className="flex justify-center items-end">
+                              <div className=" flex text-4xl items-start gap-1">
+                                <p className={`${item.class[1]} text-lg`}>Rp</p>
+                                <h1 className={`${item.class[1]} text-5xl`}>
+                                  {stand_p[0]}.
+                                </h1>
+                              </div>
+                              <div>
+                                <p className={`${item.class[1]} text-xl`}>
+                                  {stand_p[1]}
+                                </p>
+                              </div>
+                            </div>
+                            <p className={`text-black text-md font-medium`}>
+                              Per Bulan
+                            </p>
+                          </div>
+                        </div>
+                      </>
+                    )}
+                    <div className="relative mx-auto -top-5">
+                      <RadialBar
+                        speed={[item.speed.replace(" Mbps", ""), prosen]}
+                      />
+                    </div>
+                    <div className="relative -top-5 grid grid-cols-1 text-left">
+                      {item.spec.map((itemdet, idx) => {
+                        return (
+                          <div
+                            key={idx}
+                            className="py-1 flex items-center text-sm text-black"
+                          >
+                            <FaCheckCircle className="mr-2 text-green-900" />
+                            <p>{itemdet.replace("[SPEED]", item.speed)}</p>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    {/* <Link href={`https://wa.me/6287897042408`} target="blank">
+                          {" "} */}
+                    <button
+                      onClick={() => {
+                        setIsOpen(true);
+                        setchoice(`${item.id}-${isGold}`);
+                      }}
+                      className="bg-green-500 text-white px-4 py-2 rounded-lg font-bold"
+                    >
+                      Daftar Sekarang
+                    </button>
+                    {/* </Link> */}
+                  </div>
+                </motion.div>
+              ) : (
+                ""
+              );
+            })}
           </div>
-        </div> */}
+        </div>
       </div>
 
       <DialogBox
